5974f8b62f13602085e464d95669c0b4b98973f5
Nkaleth <nseguralu@gmail.com>
2024-03-05 18:33:06 -0500

    DEFINE show method in the User controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe "GET #show" do
+    before do
+      get api_v1_user_path(@user), as: :json
+      @json_response = JSON.parse(response.body)
+    end
+    
+    it 'return https success' do
+      expect(response).to have_http_status(:success)
+    end
+    
+    it 'JSON body response contains expected user email' do 
+      expect(@json_response['email']).to eq(@user.email)
+    end
   end
 end
