2c41b843bd9d23f7ea719f20d9020376ce95f15b
Nkaleth <nseguralu@gmail.com>
2024-03-05 13:20:37 -0500

    Setup Bcrypt adding has_secure_password to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_format_of :email, with: /\A\S+@\S+\z/
   validates :password_digest, presence: true
+  has_secure_password
 end
