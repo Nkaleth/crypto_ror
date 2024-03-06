85d8303d1066e80f8abcbc6581d003b392e7e1cd
Nkaleth <nseguralu@gmail.com>
2024-03-05 20:28:26 -0500

    ADDs the user create endpoint

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe "Api::V1::Users", type: :request do
       expect(@json_response['email']).to eq(@user.email)
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      let(:valid_params) { { user: attributes_for(:user) } }
+
+      it 'creates a new User' do
+        expect { post api_v1_users_path, params: valid_params }.to change(User, :count).by(1)
+      end
+
+      it 'returns a :created status' do
+        post api_v1_users_path, params: valid_params
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:invalid_params) { { user: attributes_for(:user, email: @user.email) } }
+
+      it 'does not create a new User' do
+        expect { post api_v1_users_path, params: invalid_params }.not_to change(User, :count)
+      end
+
+      it 'returns a :unprocessable_entity status' do
+        post api_v1_users_path, params: invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
