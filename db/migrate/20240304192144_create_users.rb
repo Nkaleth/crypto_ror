4cf2571156a4b99fb820ae592cfb1ccd5016e220
Nkaleth <nseguralu@gmail.com>
2024-03-11 18:48:39 -0500

    FIX User model to do not have a unique password

--- db/migrate/20240304192144_create_users.rb
+++ db/migrate/20240304192144_create_users.rb
@@ -7,6 +7,5 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.timestamps
     end
     add_index :users, :email, unique: true
-    add_index :users, :password_digest, unique: true
   end
 end
